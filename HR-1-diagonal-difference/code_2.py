5cef956fcab98cd3fd9a1b39ef5f4a250ff91127
kavinduUdhara <wmkavinduudhara@gmail.com>
2023-03-26 12:24:34 +0530

    added another approch for HR-1

--- HR-1-diagonal-difference/code_2.py
+++ HR-1-diagonal-difference/code_2.py
@@ -2,13 +2,12 @@ def diagonalDifference(arr):
     left_to_right = 0
     right_to_left = 0
     
-    for i in range(len(arr)):
-        left_to_right += (arr[i][i])
-    
+    len_of_arr = len(arr) #In general, it's a good practice to avoid calling len() in a loop if possible
     lenght = len(arr) - 1
-    for i in range(len(arr)):
+    for i in range(len_of_arr):
+        left_to_right += (arr[i][i])
         right_to_left += (arr[i][lenght])
-        lenght -= 1
+        lenght -= 1     
 
     result = abs(left_to_right - right_to_left)
     return(result)
@@ -16,6 +15,6 @@ def diagonalDifference(arr):
 print(diagonalDifference([[11, 2, 4], [4, 5, 6], [10, 8, -12]])) #15
 print(diagonalDifference([[-1, 1, -7, -8], [-10, -8, -5, -2], [0, 9, 7, -1], [4, 4, -2, 1]])) #1
 
-#3-13-2023
+#3-26-2023
 #space complexity - O(1)
 #time complexity - O(n)
