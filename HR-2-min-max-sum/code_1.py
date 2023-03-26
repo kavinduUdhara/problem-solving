519f807caf2436296d724fdd658ba495e69155d3
kavinduUdhara <wmkavinduudhara@gmail.com>
2023-03-26 13:25:37 +0530

    init first solution

---
+++ HR-2-min-max-sum/code_1.py
@@ -0,0 +1,19 @@
+def miniMaxSum(arr):
+    arr = sorted(arr)  # O(n log n)
+    sum1 = arr[1:]     # O(n)
+    sum2 = arr[:-1]    # O(n)
+
+    sum1total = 0      # O(1)
+    for n in sum1:     # O(n)
+        sum1total += n # O(1)
+
+    sum2total = 0      # O(1)
+    for n in sum2:     # O(n)
+        sum2total += n # O(1)
+
+    print("{} {}".format(sum2total, sum1total)) # O(1)
+
+print(miniMaxSum([1, 2, 3, 4, 5])) #10 14
+print(miniMaxSum([396285104, 573261094, 759641832, 819230764, 364801279])) #2093989309 2548418794
+
+#time complexity O(nlogn)
